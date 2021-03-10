295645e3450b9c667d6db2c819cce0172c664891
ganes <gustiganes@gmail.com>
2021-03-10 14:46:26 +0800

    update with check point

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-start = 21124
+start = 21850
 dataInFile = ""
+numberInfo = 1
 while(start <= 30000):
-    source = requests.get("http://bismainformatika.com/sertifikasi/cetak/cetak_form_pendaftaran.php?id=" + str(start))
+    try:
+        source = requests.get("http://bismainformatika.com/sertifikasi/cetak/cetak_form_pendaftaran.php?id=" + str(start))
+    except KeyboardInterrupt or requests.ConnectionError:
+        with open("lanjutan.txt", 'w') as wr:
+            wr.write(dataInFile)
+            wr.write("\n\n\nCheck point --> " + str(start))
+        exit()
+    # source = requests.get("http://bismainformatika.com/sertifikasi/cetak/cetak_form_pendaftaran.php?id=21757")
     soup = BeautifulSoup(source.content,'lxml')
     tables = soup.select_one('table[style="margin-left: 50px;"]')
     rows = tables.find_all('td')
     name = "".join(str(rows[2].text).split())
+
     # check data empty or not
     if name == "":
         start +=1
-        continue;
+        print("\rkosong data ke-" + str(start),end="")
+        continue
     # make each one row entered
     number_space = 0
     data = f"-------{str(start)}-------\n"
@@ -24,6 +34,8 @@ while(start <= 30000):
         data += " ".join(row.text.split())
         number_space += 1
     dataInFile += data +"\n\n"
+    print("\r\nData didapat " + str(numberInfo),end="")
     start +=1
-with open("Data3.txt",'w') as wr :
+    numberInfo +=1
+with open("lanjutan.txt",'w') as wr:
     wr.write(dataInFile)
